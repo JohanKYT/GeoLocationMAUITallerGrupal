289279dc82483adce705fc5db8b445ee8d55dd50
JohanKYT <kevin.maquis@udla.edu.ec>
2025-06-27 18:11:27 -0500

    Se agrego los ods modelos, EquipoGrupal donde ira inofrmacion de los del grupo y Recordatorio que sera una alerta de recordatiorios que se hagan

---
+++ GeoLocationMAUITallerGrupal/Models/Recordatorio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoLocationMAUITallerGrupal.Models
+{
+    public class Recordatorio
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Texto { get; set; } = string.Empty;
+        public DateTime FechaHora { get; set; } = DateTime.Now;
+        public bool Activo { get; set; } = true;
+
+
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+
+
+        public string FechaHoraFormateada => FechaHora.ToString("dd/MM/yyyy HH:mm");
+        public string TiempoRestante
+        {
+            get
+            {
+                var diferencia = FechaHora - DateTime.Now;
+
+                if (diferencia.TotalSeconds < 0)
+                    return "Vencido";
+
+                if (diferencia.TotalMinutes < 60)
+                    return $"En {diferencia.Minutes} min";
+
+                if (diferencia.TotalHours < 24)
+                    return $"En {diferencia.Hours}h {diferencia.Minutes}m";
+
+                return $"En {diferencia.Days} días";
+            }
+        }
+    }
+}

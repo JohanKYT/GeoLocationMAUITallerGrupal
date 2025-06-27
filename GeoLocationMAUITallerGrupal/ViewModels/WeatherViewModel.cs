e0d800976f98d94a6f44ef5da1458e6bece71438
JohanKYT <kevin.maquis@udla.edu.ec>
2025-06-27 17:27:49 -0500

    Se agrego Imagenes a resourses que cambien segun el clima de la ubicacion que se ponga, se modifico el viewmodel del clima para que funcione la logica del clima y se vea mas legible y bine los valores de temperatura, humedad y lluvia se agrego un scrollview en el xaml de la vista para que sea responsivo al dispositiv

--- GeoLocationMAUITallerGrupal/ViewModels/WeatherViewModel.cs
+++ GeoLocationMAUITallerGrupal/ViewModels/WeatherViewModel.cs
@@ -15,6 +15,19 @@ namespace GeoLocationMAUITallerGrupal.ViewModels
     internal class WeatherViewModel : INotifyPropertyChanged
     {
         private WeatherData _weatherData= new WeatherData();
+        private string _weatherImage;
+        public string WeatherImage
+        {
+            get => _weatherImage;
+            set
+            {
+                if (_weatherImage != value)
+                {
+                    _weatherImage = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
         public WeatherData WeatherDataInfo
         {
             get => _weatherData;
@@ -24,6 +37,10 @@ namespace GeoLocationMAUITallerGrupal.ViewModels
                 {
                     _weatherData = value;
                     OnPropertyChanged();
+                    OnPropertyChanged(nameof(TemperaturaConUnidad));
+                    OnPropertyChanged(nameof(HumedadConUnidad));
+                    OnPropertyChanged(nameof(LluviaConUnidad));
+                    SetWeatherImage();
                 }
             }
         }
@@ -36,10 +53,33 @@ namespace GeoLocationMAUITallerGrupal.ViewModels
                 Current = new CurrentWeather(),
                 CurrentUnits = new CurrentUnits()
             };
-          
+            WeatherImage = "dotnet_bot.png"; // imagen por defecto
             RecalculateWeather = new Command(async () => WeatherFromLocation());
             GetCurrentWeatherFromLocation();
         }
+        private void SetWeatherImage()
+        {
+            var temp = WeatherDataInfo?.Current?.Temperature2m ?? 0;
+            var rain = WeatherDataInfo?.Current?.Rain ?? 0;
+
+            if (rain > 0)
+                WeatherImage = "lluvia.png";
+            else if (temp >= 25)
+                WeatherImage = "soleado.png";
+            else if (temp >= 5)
+                WeatherImage = "nublado.png";
+            else
+                WeatherImage = "frio.png";
+        }
+
+        public string TemperaturaConUnidad =>
+            $"{WeatherDataInfo?.Current?.Temperature2m ?? 0} {WeatherDataInfo?.CurrentUnits?.Temperature2m ?? "°C"}";
+
+        public string HumedadConUnidad =>
+            $"{WeatherDataInfo?.Current?.RelativeHumidity2m ?? 0} {WeatherDataInfo?.CurrentUnits?.RelativeHumidity2m ?? "%"}";
+
+        public string LluviaConUnidad =>
+            $"{WeatherDataInfo?.Current?.Rain ?? 0} {WeatherDataInfo?.CurrentUnits?.Rain ?? "mm"}";
 
         public async void GetCurrentWeatherFromLocation()
         {
